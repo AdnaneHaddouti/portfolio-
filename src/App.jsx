46e7c5ba70b448f7d80d99fda313c4cde62270d7
AdnaneHD <HADDOUTI_ADNANE@emsi-edu.ma>
2023-11-16 00:21:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Experience, Hero, Navbar, Tech, Works } from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
